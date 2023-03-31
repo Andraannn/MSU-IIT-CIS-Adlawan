9e770f88c7a192c7f5a531ece595db7c46ad9b73
Andrei Adlawan <andreigabrielle.adlawan@g.msuiit.edu.ph>
2023-03-31 14:04:54 +0800

    modifications in routing

---
+++ my-project/src/routes/addUser.js
@@ -0,0 +1,14 @@
+import {React} from 'react';
+
+function AddUser(){
+
+    return (
+        <button className="w-48 bg-amber-500 text-white">Add User</button>
+        
+        
+        
+        )
+}
+
+
+export default AddUser
