93cdd27d496a6b7acc51796269a35d5ad5f35db6
Andrei Adlawan <andreigabrielle.adlawan@g.msuiit.edu.ph>
2023-04-03 10:12:17 +0800

    modified add user

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,10 @@
 import './App.css'
+import React, { useState } from "react";
 import {Routes, Route} from 'react-router-dom'
 import Accordion from './routes/accordion'
 import AddUser from './routes/addUser'
+import SimpleModal from './routes/modal'
+import AddUserModal from './routes/AddUserModal';
 
 function App() {
   
@@ -10,6 +13,8 @@ function App() {
     <Routes>
       <Route path="/accordion" element={<Accordion></Accordion>}></Route>
       <Route path="/add_user" element={<AddUser></AddUser>}></Route>
+      <Route path="/test" element={<SimpleModal></SimpleModal>}></Route>
+      <Route path="/modal_adduser" element={<AddUserModal></AddUserModal>}></Route>
     </Routes>
   </div>
   )
