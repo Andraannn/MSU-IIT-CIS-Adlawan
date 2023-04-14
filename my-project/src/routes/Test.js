121b6caea8cc8e196783bcfd7c7e3488b5c12d7d
Andrei Adlawan <andreigabrielle.adlawan@g.msuiit.edu.ph>
2023-04-14 15:29:05 +0800

    modified time

--- my-project/src/routes/Test.js
+++ my-project/src/routes/Test.js
@@ -63,7 +63,7 @@ function Testing() {
                           </div>
 
                           <div>
-                              <label for="NURSE" class="block mb-2 text-sm font-medium text-black">Time: </label>
+                              <label for="TIME" class="block mb-2 text-sm font-medium text-black">Time: </label>
                               <input
                               className="px-2 py-1 border border-black rounded-md mr-2 text-sm"
                               type="time"
@@ -85,7 +85,7 @@ function Testing() {
                           </div>
 
                           <div>
-                              <label for="NURSE" class="block mb-2 text-sm font-medium text-black">Time: </label>
+                              <label for="TIME" class="block mb-2 text-sm font-medium text-black">Time: </label>
                               <input
                               className="px-2 py-1 border border-black rounded-md mr-2 text-sm"
                               type="time"
@@ -106,7 +106,7 @@ function Testing() {
                               <input type="text" id="NURSE" class="border border-black text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder='NAME OF REGISTERED NURSE' required/>
                           </div>
                               <div>
-                                  <label for="NURSE" class="block mb-2 text-sm font-medium text-black">Time: </label>
+                                  <label for="TIME" class="block mb-2 text-sm font-medium text-black">Time: </label>
                                   <input
                                   className="px-2 py-1 border border-black rounded-md mr-2 text-sm"
                                   type="time"
