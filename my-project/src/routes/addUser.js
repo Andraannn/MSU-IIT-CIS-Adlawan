e95129c3e5310e7973946b8dd40c8f6c669aa6be
Andrei Adlawan <andreigabrielle.adlawan@g.msuiit.edu.ph>
2023-05-11 11:51:01 +0800

    modified admin side

--- my-project/src/routes/addUser.js
+++ my-project/src/routes/addUser.js
@@ -2,18 +2,110 @@ import React from "react"
 
 function AddUser(){
         const [showModal, setShowModal] = React.useState(false);
-    return (
-        <>
-         <button
-        className="bg-yellow-600 text-white active:bg-yellow-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:bg-yellow-700 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        ADD USER
-      </button>
-      {showModal ? (
-        <>
-          <div
+        
+        const renderSwitch = (param) => {
+          switch(param) {
+            case 'edit':
+              return (
+                <>
+                <div
+                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                >
+                    <div className="relative w-full my-auto mx-auto max-w-6xl">
+                        {/*content*/}
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                        {/*header*/}
+                            <div className="flex w-full items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                <h3 className="text-3xl font-semibold text-black">MANAGE USER</h3>
+                            </div>
+                        {/*body*/}
+                        <div className="relative p-5 flex-auto">
+                          <form>
+                            <div className="px-5">
+                              <label for="role"
+                                  className="block text-gray-700 font-medium mb-2 w-96"
+                                  htmlFor="role"
+                              >
+                              CHANGE ROLE
+                              </label>
+                            <select className="w-50 px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" id="role">
+                                <option>Select a Role</option>
+                                <option>Admin</option>
+                                <option>Doctor</option>
+                                <option>Nurse</option>
+                            </select>
+                            </div>
+                          </form>
+                        </div>
+                        <div className="relative p-5 flex-auto">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                          <thead class="text-xs font-bold text-gray-800 uppercase bg-gray-400 dark:bg-gray-700 dark:text-gray-400">
+                              <tr>
+                                  <th scope="col" class="p-4">
+                                  </th>
+                                  <th scope="col" class="px-6 py-3">
+                                      NAME
+                                  </th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                  <td class="w-4 p-4">
+                                      <div class="flex items-center">
+                                          <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                          <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                      </div>
+                                  </td>
+                                  <td class="px-6 py-4">VIEW MEDICAL RECORDS</td>
+                              </tr>
+                              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                  <td class="w-4 p-4">
+                                      <div class="flex items-center">
+                                          <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                          <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
+                                      </div>
+                                  </td>
+                                  <td class="px-6 py-4">TEST</td>
+                              </tr>
+                              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                  <td class="w-4 p-4">
+                                      <div class="flex items-center">
+                                          <input id="checkbox-table-search-3" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                          <label for="checkbox-table-search-3" class="sr-only">checkbox</label>
+                                      </div>
+                                  </td>
+                                  <td class="px-6 py-4">TEST 2</td>
+                              </tr>
+                          </tbody>
+                      </table>
+                        </div>
+                        {/*footer*/}
+                            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                              <button
+                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={() => setShowModal(false)}
+                              >
+                                Close
+                              </button>
+                              <button
+                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={() => setShowModal(false)}
+                              >
+                                SAVE CHANGES
+                              </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+              )
+              case 'add':
+              return (
+                <>
+                <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
@@ -21,7 +113,7 @@ function AddUser(){
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex w-96 items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
+                  <h3 className="text-3xl font-semibold text-black">
                     ADD USER
                   </h3>
                   <button
@@ -81,19 +173,31 @@ function AddUser(){
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Save Changes
+                    CONFIRM
                   </button>
                 </div>
               </div>
             </div>
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
-        
+                </>
+              )
+              }
+        }
+              
+    return (
+        <>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">        
     <div class="pb-4 pt-6 px-2 bg-white dark:bg-gray-900">
-        <label for="table-search" class="sr-only">Search</label>
+        <label for="table-search" class="sr-only">Search</label><span>
+        <button
+        className="bg-yellow-600 text-white active:bg-yellow-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:bg-yellow-700 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal('add')}
+      >
+        ADD USER
+      </button>  
+        </span>
         <div class="relative mt-2">
             <div class="absolute inset-y-1 left-0 flex items-center pl-3 pointer-events-none">
                 <svg class="w-5 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
@@ -102,7 +206,7 @@ function AddUser(){
         </div>
     </div>
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead class="text-xs font-bold text-gray-800 uppercase bg-gray-400 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="p-4">
                     <div class="flex items-center">
@@ -142,7 +246,7 @@ function AddUser(){
                     DOCTOR
                 </td>
                 <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" onClick={() => setShowModal('edit')}>Edit</a>
                     <a href="#" class="px-5 font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
                 </td>
             </tr>
@@ -189,6 +293,7 @@ function AddUser(){
                 </td>
             </tr>
         </tbody>
+        {renderSwitch(showModal)}
     </table>
 </div>
 </>
--- my-project/src/routes/testing.js
+++ my-project/src/routes/testing.js
@@ -1,80 +1,57 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const data = [
-  {
-    id: 1,
-    fullName: 'John Doe',
-    patientType: 'Inpatient',
-    contactNumber: '+1 555-1234'
-  },
-  {
-    id: 2,
-    fullName: 'Jane Doe',
-    patientType: 'Outpatient',
-    contactNumber: '+1 555-5678'
-  },
-  // ... Add more data here
+  { id: 1, email: 'john@example.com', fullName: 'John Doe', role: 'admin', dateCreated: '2022-01-01' },
+  { id: 2, email: 'jane@example.com', fullName: 'Jane Smith', role: 'user', dateCreated: '2022-02-01' },
+  { id: 3, email: 'jim@example.com', fullName: 'Jim Johnson', role: 'user', dateCreated: '2022-03-01' },
+  { id: 4, email: 'jill@example.com', fullName: 'Jill Jackson', role: 'user', dateCreated: '2022-04-01' },
+  { id: 5, email: 'jack@example.com', fullName: 'Jack Johnson', role: 'user', dateCreated: '2022-05-01' },
+  { id: 6, email: 'judy@example.com', fullName: 'Judy Jackson', role: 'user', dateCreated: '2022-06-01' },
+  { id: 7, email: 'joe@example.com', fullName: 'Joe Jackson', role: 'user', dateCreated: '2022-07-01' },
+  { id: 8, email: 'joe@example.com', fullName: 'Joe Jackson', role: 'user', dateCreated: '2022-07-01' },
+
+  // Add more data as needed
+];
+
+const columns = [
+  { key: 'id', name: 'ID' },
+  { key: 'email', name: 'Email' },
+  { key: 'fullName', name: 'Full Name' },
+  { key: 'role', name: 'Role' },
+  { key: 'dateCreated', name: 'Date Created' },
 ];
 
 const TestingCode = () => {
-  const [sortedData, setSortedData] = useState(data);
-
-  const handleSort = () => {
-    const sorted = [...sortedData].sort((a, b) => a.fullName.localeCompare(b.fullName));
-    setSortedData(sorted);
-  };
-
   return (
-    <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    ID Number
+    <div className="flex flex-col h-full">
+      <div className={`flex-grow ${data.length > 5 ? 'overflow-y-auto' : ''}`}>
+        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                {columns.map((column) => (
+                  <th
+                    key={column.key}
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    {column.name}
                   </th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Full Name
-                  </th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Patient Type
-                  </th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Contact Number
-                  </th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    <button
-                      onClick={handleSort}
-                      className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                    >
-                      Sort Alphabetically
-                    </button>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {sortedData.map((item) => (
-                  <tr key={item.id}>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                      {item.id}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {item.fullName}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {item.patientType}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {item.contactNumber}
-                    </td>
-                    <td></td>
-                  </tr>
                 ))}
-              </tbody>
-            </table>
-          </div>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {data.map((item) => (
+                <tr key={item.id}>
+                  {columns.map((column) => (
+                    <td key={column.key} className="px-6 py-4 whitespace-nowrap">
+                      {item[column.key]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
